fbeb1569a500a7ff36614484015c333b46f61e53
jessekauppila <jesse.kauppila@gmail.com>
2025-09-09 23:05:38 -0700

    Starting test of ClosestWebcams

---
+++ app/components/Map/hooks/useClosestWebcams.test.ts
@@ -0,0 +1,8 @@
+import { useClosestWebcams } from './useClosestWebcams';
+import { describe, it, expect } from 'vitest';
+
+describe('useClosestWebcams', () => {
+  it('finds closest webCam', () => {});
+  it('creates ordered array of webCams', () => {});
+  it('finds closest location', () => {});
+});
