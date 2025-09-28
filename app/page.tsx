486af80c5a1adc3fde54311a57c4ec0c70bd42c9
jessekauppila <jesse.kauppila@gmail.com>
2025-09-27 18:21:05 -0700

    Added useLoadTerminatorWebcams to top of app to bring in the data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import SimpleMap from './components/Map/SimpleMap';
-
-//import
+import { useLoadTerminatorWebcams } from '@/app/store/useLoadTerminatorWebcams';
 
 export default function Home() {
-  // Bellingham, Washington location
+  // Bellingham, Washington location need to put in user's location eventually
   const userLocation = { lat: 48.7519, lng: -122.4787 };
+
+  // Fetches sunrise/sunset webcams from database and stores in Zustand
+  // Automatically refreshes every 60 seconds using SWR
+  // Splits webcams into sunrise[] and sunset[] arrays by phase
+  useLoadTerminatorWebcams();
 
   return (
     <main className="relative w-full">
